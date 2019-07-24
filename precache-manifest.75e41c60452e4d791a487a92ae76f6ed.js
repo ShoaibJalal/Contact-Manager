1754405f418bfec3bd40967abbb571162ae7759f
ShoaibJalal <syedshoaibjalal@outlook.com>
2019-07-24 15:44:56 +0300

    Updates

---
+++ precache-manifest.75e41c60452e4d791a487a92ae76f6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780fa69d86133a9c1e902ebf8fa5a352",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "28c433d02ad2cd877159",
+    "url": "/contact-manager/static/css/2.b9dc5a1e.chunk.css"
+  },
+  {
+    "revision": "bf4a2ecdd30a5b7fc258",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "28c433d02ad2cd877159",
+    "url": "/contact-manager/static/js/2.5eb7b9bf.chunk.js"
+  },
+  {
+    "revision": "bf4a2ecdd30a5b7fc258",
+    "url": "/contact-manager/static/js/main.38434f7d.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
